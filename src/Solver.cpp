d6a942c2d00daac07d2d1a8cce8300aafcfcbc83
chloewen <chloewen@andrew.cmu.edu>
2023-04-16 19:54:55 -0400

    implemented Solver, NOT TESTED

---
+++ src/Solver.cpp
@@ -0,0 +1,30 @@
+#include "Solver.h"
+#include "Board.h"
+
+Solver::Solver(Board startingBoard) {
+  this->startingBoard = startingBoard;
+}
+
+Board solveBFS() {
+  std::set<Board> explored;
+  std::set<Board> frontierSet; 
+  std::queue<Board> frontier;
+  frontier.push(this->startingBoard);
+  while (true) {
+    if (frontier.empty()) return NULL;
+    Board currBoard = frontier.pop();
+    if (currBoard.isSolved()) return currBoard.pastActions; 
+    if (!explored.contains(currBoard)) {
+      explored.insert(currBoard);
+      std::vector<Board> nextBoards = currBoard.getNextBoards;
+      for (int i = 0; i < nextBoards.size(); i++) {
+        Board nextBoard = nextBoards[i];
+        if (!explored.contains(nextBoard) && !frontierSet.contains(nextBoard)) {
+          frontierSet.push(nextBoard);
+          frontier.push(nextBoard);
+        } 
+      }
+    }
+  }
+  return NULL;
+}
---
+++ src/Solver.h
@@ -0,0 +1,10 @@
+#ifndef UNBLOCK_ME_SOLVER_SOLVER_H
+#define UNBLOCK_ME_SOLVER_SOLVER_H
+
+class Solver {
+public:
+  Solver(Board startingBoard);
+  Board solveBFS();
+}
+
+#endif //UNBLOCK_ME_SOLVER_SOLVER_H
